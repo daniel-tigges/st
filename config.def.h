aba8bb0d54cb07cbecd14be59cd02f19aefa2735
Daniel Tigges <danieltigges@googlemail.com>
2019-05-08 22:27:15 +0200

    Change config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -168,7 +168,7 @@ MouseKey mkeys[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (Mod1Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -186,6 +186,21 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_Page_Up,     kscrollup,      {.i = -1} },
+        { MODKEY,               XK_Page_Down,   kscrolldown,    {.i = -1} },
+        { MODKEY,               XK_k,           kscrollup,      {.i =  1} },
+        { MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
+        { MODKEY,               XK_Up,          kscrollup,      {.i =  1} },
+        { MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
+        { MODKEY,               XK_u,           kscrollup,      {.i = -1} },
+        { MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
+        { TERMMOD,              XK_Up,          zoom,           {.f = +1} },
+        { TERMMOD,              XK_Down,        zoom,           {.f = -1} },
+        { TERMMOD,              XK_K,           zoom,           {.f = +1} },
+        { TERMMOD,              XK_J,           zoom,           {.f = -1} },
+        { TERMMOD,              XK_U,           zoom,           {.f = +2} },
+        { TERMMOD,              XK_D,           zoom,           {.f = -2} },
+
 };
 
 /*
